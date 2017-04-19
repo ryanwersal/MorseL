a11b1c46197fc8c053133ec2942f024b99265f10
Austin Hanson <austin.hanson@zuerchertech.com>
2017-04-19 08:51:07 -0500

    Restructuring, adding AsyncWebSocketClient.Tests
    - Removing system.net.websockets
    - Removing a couple files
    - Adding src/test/sample folders to solution(s)
    - Adding AsyncWebSocketClient.Tests project

---
+++ test/AsyncWebSocketClient.Tests/WebSocketClientTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AsyncWebSocketClient.Tests
+{
+    public class WebSocketClientTests
+    {
+        [Fact]
+        [Trait("project", "AsyncWebSocketClient")]
+        public void ConnectTimesOutAfterFiveSeconds()
+        {
+            WebSocketClient client = new WebSocketClient("no.where.com");
+            var time = DateTime.Now;
+            var task = client.ConnectAsync();
+            task.Wait();
+            Assert.False(task.IsCompleted);
+            Assert.True(task.IsFaulted);
+        }
+    }
+}
