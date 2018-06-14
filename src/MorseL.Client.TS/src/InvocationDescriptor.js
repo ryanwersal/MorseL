fa7d21bf4fe5bf42a8dca465c7ad474ed68047c6
Austin Hanson <berdon@gmail.com>
2018-06-14 10:04:01 -0500

    ISS-38 Fixing typescript client (#39)

--- src/MorseL.Client.TS/src/InvocationDescriptor.js
+++ src/MorseL.Client.TS/src/InvocationDescriptor.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var InvocationDescriptor = (function () {
     function InvocationDescriptor(methodName, args) {
+        this.Id = InvocationDescriptor.CurrentId++;
         this.MethodName = methodName;
         this.Arguments = args;
     }
     return InvocationDescriptor;
 }());
+InvocationDescriptor.CurrentId = 1;
 exports.InvocationDescriptor = InvocationDescriptor;
 //# sourceMappingURL=InvocationDescriptor.js.map
--- src/MorseL.Client.TS/src/InvocationDescriptor.ts
+++ src/MorseL.Client.TS/src/InvocationDescriptor.ts
@@ -1,8 +1,11 @@
 export class InvocationDescriptor {
+    private static CurrentId = 1;
+    public Id: number;
     public MethodName: string;
     public Arguments: Array<any>;
 
     constructor(methodName: string, args: any[]) {
+        this.Id = InvocationDescriptor.CurrentId++;
         this.MethodName = methodName;
         this.Arguments = args;
     }
